77c28cca2c40192df79f616b803c93405bc74f6d
Talaa Zina <talazina@gmail.com>
2020-06-07 18:39:26 +0100

    add express session

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 
 const bodyParser = require('body-parser');
 
+const session = require('express-session')
+
 
 const port = 8080;
 
@@ -18,16 +20,31 @@ app.use(express.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: false })); // for parsing application/x-www-form-urlencoded
 app.use(bodyParser.json());
 
+app.use(session({
+  secret: '123456',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
+
+
 //Route to home page
 app.get('/', (req, res) => {
-  res.render('index')
-});
-
-app.post('/', (req, res) => {
-  if (req.body.message === undefined || req.body.message === ''){
-  	 res.render('index', { mesg: 'Your message is empty' })
-  }	
+  if (req.session.error){
+    res.locals.error = req.session.error
+    req.session.error = undefined
+    
+  }
+      res.render('index')
   
+  });
+
+app.post('/',(req,res) => {
+  if (req.body.message === undefined || req.body.message === ''){
+    req.session.error = "some thing went wrong"
+    res.redirect('/')
+  }
 })
+
 
 app.listen(port)
